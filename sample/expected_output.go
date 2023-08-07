cd02c9f7a6d0cd28eda1e13de0d9ff9393ea8b1e
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2023-08-07 17:33:18 +0300

    add expected output that really represents what we're trying to achieve here

--- sample/expected_output.go
+++ sample/expected_output.go
@@ -6,17 +6,40 @@ import (
 	gojason "github.com/isaac-weisberg/go-jason"
 )
 
-var a = 3
+func newAddMoneyRequest(bytes []byte) (*addMoneyRequest, error) {
+	var j = errors.Join
+	var e = errors.New
 
-func newAddMoneyRequest(jsonString string) (*addMoneyRequest, error) {
-	var rootValue, err = gojason.Parse(jsonString)
+	rootValue, err := gojason.Parse(bytes)
 	if err != nil {
-		return nil, errors.Join(errors.New("parsing json failed"), err)
+		return nil, j(e("parsing json failed"), err)
 	}
 
 	rootObject, err := rootValue.AsObject()
 	if err != nil {
-		return nil, errors.Join(errors.New("interpreting root value as an object failed"), err)
+		return nil, j(e("interpreting root value as an object failed"), err)
 	}
 
+	var stringKeyValues = rootObject.StringKeyedKeyValuesOnly()
+
+	var exists bool
+	valueForAmountKey, exists := stringKeyValues["amount"]
+	if !exists {
+		return nil, j(e("value not found for key 'amount'"))
+	}
+	valueForAmountKeyAsNumberValue, err := valueForAmountKey.AsNumber()
+	if err != nil {
+		return nil, j(e("parsing value for key 'amount' failed"), err)
+	}
+
+	parsedInt64ForAmountKey, err := valueForAmountKeyAsNumberValue.ParseInt64()
+	if err != nil {
+		return nil, j(e("parsing value for key 'amount' failed"), err)
+	}
+
+	var resultingStructAddMoneyRequest = addMoneyRequest{
+		amount: *parsedInt64ForAmountKey,
+	}
+
+	return &resultingStructAddMoneyRequest, nil
 }
